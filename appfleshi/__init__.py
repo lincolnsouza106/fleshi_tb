05ab28152ea647ad46e15480c711b05c2da0c1be
tiagoNevesB04 <tiago.neves@docente.senai.br>
2025-12-09 16:14:03 -0300

    setup: update to deploy

--- appfleshi/__init__.py
+++ appfleshi/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datafleshi.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SECRET_KEY'] = 'secret'
 app.config['UPLOAD_FOLDER'] = 'static/posts_photos'
 
